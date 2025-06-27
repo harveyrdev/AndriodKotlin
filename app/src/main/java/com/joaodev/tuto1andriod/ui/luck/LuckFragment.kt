a8c234bbca7a430236bc288f7298cd5af1493834
Bryan Joao Muentes Vivero <134031526+harveyrdev@users.noreply.github.com>
2025-06-27 17:53:08 -0500

    animation complete

--- app/src/main/java/com/joaodev/tuto1andriod/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/joaodev/tuto1andriod/ui/luck/LuckFragment.kt
@@ -1,20 +1,35 @@
 package com.joaodev.tuto1andriod.ui.luck
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.joaodev.tuto1andriod.R
 import com.joaodev.tuto1andriod.databinding.FragmentLuckBinding
 import com.joaodev.tuto1andriod.ui.horoscopo.HoroscopoViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Random
+
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
-    private  val luckViewModel by viewModels<LuckViewModel>()
+    private val luckViewModel by viewModels<LuckViewModel>()
     private var _binging: FragmentLuckBinding? = null
     private val binding get() = _binging!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUi()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,5 +39,102 @@ class LuckFragment : Fragment() {
         return binding.root
     }
 
+    private fun initUi() {
+        initListeners()
+    }
 
+    private fun initListeners() {
+        binding.ivRulete.setOnClickListener {
+            spinRulete()
+        }
+    }
+
+    private fun spinRulete() {
+        val ramdow = Random()
+        val degress = ramdow.nextInt(1440) + 360
+
+
+        var animaros =
+            ObjectAnimator.ofFloat(binding.ivRulete, View.ROTATION, 0f, degress.toFloat())
+
+        animaros.duration = 2000
+        animaros.interpolator = DecelerateInterpolator()
+        animaros.doOnEnd {
+            slideCard()
+        }
+
+        animaros.start()
+    }
+
+    private fun slideCard() {
+        val splideUpAnimtion = AnimationUtils.loadAnimation(requireContext(), R.anim.slideup)
+        splideUpAnimtion.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                binding.ivReverse.isVisible = true
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                growCard()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+        })
+
+        binding.ivReverse.startAnimation(splideUpAnimtion)
+
+
+    }
+
+    private fun growCard() {
+        val growAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.growcard)
+        growAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                binding.ivReverse.isVisible=false
+                showPremonition()
+
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+        })
+        binding.ivReverse.startAnimation(growAnimation)
+    }
+
+    private fun showPremonition() {
+
+        val dissperAnimation=AlphaAnimation(1.0f,0.0f)
+        dissperAnimation.duration=200
+
+        val apperaAnimation=AlphaAnimation(0.0f,1.0f)
+        apperaAnimation.duration=1000
+
+        dissperAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+
+                binding.preview.isVisible=false
+                binding.prediccion.isVisible=true
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+        })
+
+        binding.preview.startAnimation(dissperAnimation)
+        binding.prediccion.startAnimation(apperaAnimation)
+    }
 }
